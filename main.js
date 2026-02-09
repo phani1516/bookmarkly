2a5d643bf516274c410caba8b4740ff2ca3df2f1
phani1516 <61675274+phani1516@users.noreply.github.com>
2026-02-08 21:39:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://dupnqfzzhcxtzewnlaft.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1cG5xZnp6aGN4dHpld25sYWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MDM2MDMsImV4cCI6MjA4NjE3OTYwM30.404qW1CAlHacEVz75KAuZwrYN3cZUvXYHOKAX2L6lvk";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
