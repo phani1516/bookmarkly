86bf58a25694dbb64ec53c25d87a303a47d6d7a8
phani1516 <61675274+phani1516@users.noreply.github.com>
2026-02-09 15:22:33 -0600

    Delete src directory
    Delete src directory for New proj file

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://dupnqfzzhcxtzewnlaft.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1cG5xZnp6aGN4dHpld25sYWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MDM2MDMsImV4cCI6MjA4NjE3OTYwM30.404qW1CAlHacEVz75KAuZwrYN3cZUvXYHOKAX2L6lvk';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
